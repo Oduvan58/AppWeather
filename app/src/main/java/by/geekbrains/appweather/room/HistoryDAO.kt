9483034b04c410a16c3817b02430783509f48913
Oduvan58 <yancheuskipavel@gmail.com>
2022-04-13 09:46:13 +0300

    Add package room

---
+++ app/src/main/java/by/geekbrains/appweather/room/HistoryDAO.kt
@@ -0,0 +1,21 @@
+package by.geekbrains.appweather.room
+
+import androidx.room.*
+
+interface HistoryDAO {
+
+    @Query("SELECT * FROM HistoryEntity")
+    fun all(): List<HistoryEntity>
+
+    @Query("SELECT * FROM HistoryEntity WHERE name LIKE :name")
+    fun getDataByWord(name: String): List<HistoryEntity>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insert(entity: HistoryEntity)
+
+    @Update
+    fun update(entity: HistoryEntity)
+
+    @Delete
+    fun delete(entity: HistoryEntity)
+}
---
+++ app/src/main/java/by/geekbrains/appweather/room/HistoryEntity.kt
@@ -0,0 +1,13 @@
+package by.geekbrains.appweather.room
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class HistoryEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long,
+    val name: String,
+    val temperature: Int,
+    val condition: String,
+)
