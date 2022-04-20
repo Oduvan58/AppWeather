378a8a77ee3864a00cfbc1b289160fd46946dff0
Oduvan58 <yancheuskipavel@gmail.com>
2022-04-20 10:18:48 +0300

    Tried to connect maps

---
+++ app/src/main/java/by/geekbrains/appweather/view/maps/MapsFragment.kt
@@ -0,0 +1,165 @@
+package by.geekbrains.appweather.view.maps
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.graphics.Color
+import android.location.Address
+import android.location.Geocoder
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import by.geekbrains.appweather.R
+import by.geekbrains.appweather.databinding.FragmentMapsBinding
+import by.geekbrains.appweather.databinding.FragmentMapsMainBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.*
+import java.io.IOException
+
+class MapsFragment : Fragment() {
+
+    private var _binding: FragmentMapsMainBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var map: GoogleMap
+    private val markers: ArrayList<Marker> = arrayListOf()
+
+    companion object {
+        fun newInstance() = MapsFragment()
+    }
+
+    private val callback = OnMapReadyCallback { googleMap ->
+        map = googleMap
+        val initPlace = LatLng(52.52000659999999, 13.404953999999975)
+        googleMap.addMarker(MarkerOptions().position(initPlace)
+            .title(getString(R.string.marker_start)))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(initPlace))
+        googleMap.setOnMapClickListener { latlng ->
+            getAddressAsync(latlng)
+            addMarkerToArray(latlng)
+            drawLine()
+        }
+        activateMyLocation(googleMap)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        _binding = FragmentMapsMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(callback)
+        initSearchByAddress()
+    }
+
+    private fun initSearchByAddress() {
+        binding.searchButton.setOnClickListener {
+            val geoCoder = Geocoder(it.context)
+            val searchText = binding.searchAddressEditText.text.toString()
+            Thread {
+                try {
+                    val addresses = geoCoder.getFromLocationName(searchText, 1)
+                    if (addresses.size > 0) {
+                        goToAddress(addresses, it, searchText)
+                    }
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            }.start()
+        }
+    }
+
+    private fun goToAddress(
+        addresses: MutableList<Address>,
+        view: View,
+        searchText: String,
+    ) {
+        val location = LatLng(
+            addresses[0].latitude,
+            addresses[0].longitude
+        )
+        view.post {
+            setMarker(location, searchText)
+            map.moveCamera(
+                CameraUpdateFactory.newLatLngZoom(
+                    location,
+                    15f
+                )
+            )
+        }
+    }
+
+    private fun getAddressAsync(location: LatLng) {
+        context?.let {
+            val geoCoder = Geocoder(it)
+            Thread {
+                try {
+                    val addresses =
+                        geoCoder.getFromLocation(location.latitude, location.longitude, 1)
+                    binding.textAddressTextView.post {
+                        binding.textAddressTextView.text = addresses[0].getAddressLine(0)
+                    }
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            }.start()
+        }
+    }
+
+    private fun addMarkerToArray(location: LatLng) {
+        val marker = setMarker(location, markers.size.toString())
+        marker?.let { markers.add(it) }
+    }
+
+    private fun setMarker(
+        location: LatLng,
+        searchText: String,
+    ): Marker? {
+        return map.addMarker(
+            MarkerOptions()
+                .position(location)
+                .title(searchText)
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ROSE))
+        )
+    }
+
+    private fun drawLine() {
+        val last: Int = markers.size - 1
+        if (last >= 1) {
+            val previous: LatLng = markers[last - 1].position
+            val current: LatLng = markers[last].position
+            map.addPolyline(
+                PolylineOptions()
+                    .add(previous, current)
+                    .color(Color.RED)
+                    .width(5f)
+            )
+        }
+    }
+
+    private fun activateMyLocation(googleMap: GoogleMap) {
+        context?.let {
+            val isPermissionGranted =
+                ContextCompat.checkSelfPermission(it, Manifest.permission.ACCESS_FINE_LOCATION) ==
+                        PackageManager.PERMISSION_GRANTED
+            googleMap.isMyLocationEnabled = isPermissionGranted
+            googleMap.uiSettings.isMyLocationButtonEnabled = isPermissionGranted
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
